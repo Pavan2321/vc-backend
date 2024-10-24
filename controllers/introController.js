ff27f48550f8ef4ccc2172ad47d1679053b1cd0a
pavan kumar <pavankumar230799@gmail.com>
2024-10-24 10:58:40 +0530

    update access link for drive

--- controllers/introController.js
+++ controllers/introController.js
@@ -4,10 +4,10 @@ const introController = (req, res) => {
         <center>
           <h1>WELCOME TO E-COMMERCE CRUD API's</h1>
           <p>Get postman collections bellow for testing local and deployed all api's</p>
-          <a href='https://drive.google.com/file/d/1NPcfil5tKIhV_d8Za62-t1xrijs5Pts0/view?usp=drive_link' target="_blank" >Postman Collection for localhost API's</a>
+          <a href='https://drive.google.com/file/d/1NPcfil5tKIhV_d8Za62-t1xrijs5Pts0/view?usp=sharing' target="_blank" >Postman Collection for localhost API's</a>
           <br />
           <br />
-          <a href='https://drive.google.com/file/d/1vhzFGgA6lP2I86B9AVoV9Hk6wqUyO1nY/view?usp=drive_link' target="_blank" >Postman Collection for deployed API's</a>
+          <a href='https://drive.google.com/file/d/1vhzFGgA6lP2I86B9AVoV9Hk6wqUyO1nY/view?usp=sharing' target="_blank" >Postman Collection for deployed API's</a>
         </center>
       </div>
     `);
